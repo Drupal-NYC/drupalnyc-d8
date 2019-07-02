72695de266bf65f661e4117fe0ac0581ec35e3c2
Shawn P. Duncan <sduncan@digitalpulp.com>
2019-07-02 07:09:00 -0400

    Working on theme build pipeline

--- web/themes/custom/drupalnyc/gulpfile.js
+++ web/themes/custom/drupalnyc/gulpfile.js
@@ -1,59 +1,126 @@
-// Basic gulpfile for compiling SCSS to CSS
-// Will inject changes as they happen via browser-sync
-// For Gulp version 4 and above only
-// See the README for more information
-
-'use strict';
-
+/* eslint-disable */
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var del = require('del');
+var rename = require('gulp-rename');
+var eslint = require('gulp-eslint');
 var sass = require('gulp-sass');
+var sassGlob = require('gulp-sass-glob');
+var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
+var livereload = require('gulp-livereload');
+var sassLint = require('gulp-sass-lint');
 var autoprefixer = require('gulp-autoprefixer');
-var importer = require('node-sass-globbing');
-var plumber = require('gulp-plumber');
-var browserSync = require('browser-sync').create();
 
-var sass_config = {
-  importer: importer,
-  includePaths: [
-     // These are the paths to the npm modules installed globally.
-     // If you have installed the modules locally, or aren't using Mac OSX,
-     // these paths will need to be adjusted.
-    '/usr/local/lib/node_modules/breakpoint-sass/stylesheets/',
-    '/usr/local/lib/node_modules/compass-mixins/lib/',
+var paths = {
+  scripts: [
+    'js/**/*.js',
+    '!js/**/*.min.js'
+  ],
+  sass: {
+    main: 'sass/bones_subtheme.styles.scss',
+    watch: 'sass/scss/**/*'
+  },
+  css: {
+    root: 'css'
+  },
+  clean: [
+    'css/styles.css',
+    'css/**/*.css.map',
+    'js/**/*.min.js'
   ]
 };
 
-gulp.task('browser-sync', function(done) {
-    browserSync.init({
-        injectChanges: true,
-        // Update the proxy location to the local site location to see
-        // BrowserSync changes reload automatically
-        // Do not include http
-        proxy: "YOUR-LOCAL-SITE:PORT"
-    });
-    gulp.watch('./sass/*.scss', gulp.task(['sass']));
-    gulp.watch(['./css/.styles.css']).on('change', browserSync.reload);
-    done();
+gulp.task('clean', function () {
+  'use strict';
+  return del(paths.clean);
 });
 
-gulp.task('sass', function (done) {
-  gulp.src('./sass/**/*.scss')
-    .pipe(plumber())
-    .pipe(sourcemaps.init())
-    .pipe(sass(sass_config).on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: [
-        'last 2 versions',
-        'android 4',
-        'opera 12'
-      ]
-    }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./css'));
-    done();
+gulp.task('js-lint', function () {
+  'use strict';
+  return gulp.src(paths.scripts)
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(eslint.failAfterError());
 });
 
-var build = gulp.series('browser-sync', ['sass']);
+gulp.task('sass-lint', function () {
+  'use strict';
+  return gulp.src(paths.sass.watch)
+      .pipe(sassLint({
+        files: {
+          ignore: [
+            'bower_components/**/*.scss',
+            'styles/scss/normalize/**',
+            'styles/scss/abstractions/_fonts.scss',
+            'styles/scss/styles.scss'
+          ]
+        },
+        rules: {
+          'no-ids': 0,
+          'nesting-depth': [1, {'max-depth': 4}],
+          'no-qualifying-elements': [1, {'allow-element-with-class': true}],
+          'force-element-nesting': 0,
+          'force-pseudo-nesting': 0,
+          'property-sort-order': 0,
+          'no-vendor-prefixes': 0,
+          'mixins-before-declarations': [1, {exclude: ['media']}],
+          'placeholder-in-extend': 0,
+          'single-line-per-selector': 0,
+          'no-misspelled-properties': [1, {'extra-properties': ['-webkit-overflow-scrolling']}],
+          'class-name-format': 0
+        }
+      }))
+      .pipe(sassLint.format())
+      .pipe(sassLint.failOnError());
+});
 
-gulp.task('default', build);
+
+gulp.task('lint', gulp.series(gulp.parallel('js-lint', 'sass-lint')));
+
+gulp.task('compress', function () {
+  'use strict';
+  return gulp.src(paths.scripts)
+      .pipe(rename({suffix: '.min'}))
+      .pipe(uglify())
+      .pipe(gulp.dest('js/dist'));
+});
+
+gulp.task('sass', function () {
+  'use strict';
+  return gulp.src(paths.sass.main)
+      .pipe(sassGlob())
+      .pipe(sourcemaps.init())
+      .pipe(sass({
+        includePaths: [
+          'node_modules/support-for/sass'
+        ]
+      }).on('error', sass.logError))
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions']
+      }))
+      .pipe(sourcemaps.write('./maps'))
+      .pipe(gulp.dest(paths.css.root))
+      .pipe(livereload());
+});
+
+gulp.task('sourcemaps', function () {
+  'use strict';
+  return gulp.src(paths.sass.main)
+      .pipe(sassGlob())
+      .pipe(sourcemaps.init())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(paths.css.root));
+});
+
+gulp.task('build', gulp.series(gulp.parallel('sass', 'compress')));
+
+gulp.task('watch', gulp.series(gulp.parallel('build'), function () {
+  'use strict';
+  livereload.listen();
+  gulp.watch(paths.sass.watch, gulp.parallel('sass'));
+  gulp.watch(paths.scripts, gulp.parallel('compress'));
+}));
+
+gulp.task('default', gulp.parallel('build'));
