51be00c5bae9eceb1da581c1c7a6312ff2944ccf
Shawn P. Duncan <sduncan@digitalpulp.com>
2019-08-24 14:04:56 -0400

    Working on bg image

---
+++ web/modules/custom/dnyc_header_block/src/Plugin/Block/HeaderBlock.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Drupal\maw_luminate\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\node\Entity\Node;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a header content block.
+ *
+ * @Block(
+ *   id = "dnyc_header_content",
+ *   admin_label = @Translation("Header Content"),
+ *   category = @Translation("Custom")
+ * )
+ */
+class HeaderBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The current route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * Injected service.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+
+  /**
+   * Constructs a new DonationFlowContentBlock instance.
+   *
+   * @param array $configuration
+   *   The plugin configuration, i.e. an array with configuration values keyed
+   *   by configuration option name. The special key 'context' may be used to
+   *   initialize the defined contexts by setting it to an array of context
+   *   values keyed by context names.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The current route match.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   The renderer service.
+   */
+  public function __construct(array $configuration,
+                              $plugin_id,
+                              $plugin_definition,
+                              EntityTypeManagerInterface $entity_type_manager,
+                              RouteMatchInterface $route_match,
+                              RendererInterface $renderer) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entity_type_manager;
+    $this->routeMatch = $route_match;
+    $this->renderer = $renderer;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('current_route_match'),
+      $container->get('renderer')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    /** @var \Drupal\node\Entity\Node $donationTheme */
+    $donationTheme = $this->routeMatch->getParameter('donation_theme');
+
+    $build = [];
+    if (!empty($donationTheme) && $donationTheme instanceof Node) {
+      $build = $this->entityTypeManager->getViewBuilder('node')
+        ->view($donationTheme, 'full');
+    }
+    $this->renderer->addCacheableDependency($build, $donationTheme);
+    $build['#cache']['contexts'][] = 'url.path';
+    return $build;
+  }
+
+}
