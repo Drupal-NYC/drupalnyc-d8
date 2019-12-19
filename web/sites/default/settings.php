fe379a83f24c42e4fd66ff41343c1a92d8668bf7
Shawn P. Duncan <code@sd.shawnduncan.org>
2019-12-18 21:16:51 -0500

    Update core to 8.8.1

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -768,7 +768,7 @@ if (file_exists(__DIR__ . '/settings.pantheon.php')) {
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
-$config_directories['sync'] = '../config';
+$settings['config_sync_directory'] = '../config';
 
 // Do not remove the following line.
 /* Settings added by robo setup:drupal */
