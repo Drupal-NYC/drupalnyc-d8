b6fcdd300947ceff2a52cdddab0b5e4e36fa8710
Shawn Duncan <duncans3@mskcc.org>
2022-11-13 08:26:36 -0500

    Upgrade all modules to last D8 and prepare to re-theme

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -799,3 +799,9 @@ else {
   }
 }
 
+
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  require $ddev_settings;
+}
