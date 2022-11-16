f12de324b819ec6f9f90b5b842a9f43ce3a5e70c
Shawn Duncan <duncans3@mskcc.org>
2022-11-16 06:47:36 -0500

    simplify deployment automation

--- web/private/hooks/deploy.php
+++ web/private/hooks/deploy.php
@@ -9,22 +9,10 @@
 echo "Switch to maintenance mode...\n";
 passthru('drush sset system.maintenance_mode 1 -y');
 echo "In maintenance mode.\n";
-// Clear cache.
-echo "Clear all cache bins\n";
-passthru('drush cr');
-echo "Cache clear complete.\n";
-// Update database.
-echo "Apply database updates\n";
-passthru('drush updb -y');
-echo "Database updates complete.\n";
-// Config Import.
-echo "Import configuration\n";
-passthru('drush cim sync -y');
-echo "Configuration import complete.\n";
-// Clear cache.
-echo "Clear all cache bins\n";
-passthru('drush cr');
-echo "Cache clear complete.\n";
+// Run drush deploy.
+echo "Process all updates\n";
+passthru('drush deploy');
+echo "Updates complete.\n";
 // Maintenance off.
 echo "Come out of maintenance mode\n";
 passthru('drush sset system.maintenance_mode 0 -y');
