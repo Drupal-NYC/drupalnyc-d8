7ac7143c7943cf921dac1a9422705dc3d086d28a
shawn <code@sd.shawnduncan.org>
2023-03-25 15:44:03 -0400

    Update deploy hooks v.2

--- web/private/hooks/deploy.php
+++ web/private/hooks/deploy.php
@@ -4,17 +4,20 @@
  * @file
  * Pantheon hooks.
  */
-$dir = dirname(DRUPAL_ROOT);
-$drush = "$dir/vendor/bin/drush";
+
 // Maintenance on.
 echo "Switch to maintenance mode...\n";
-passthru("$drush sset system.maintenance_mode 1 -y");
+passthru("drush sset system.maintenance_mode 1 -y");
 echo "In maintenance mode.\n";
 // Run drush deploy.
 echo "Process all updates\n";
-passthru("$drush deploy");
+passthru("drush updatedb --no-cache-clear");
+passthru("drush cache:rebuild");
+passthru("drush config:import");
+passthru("cache:rebuild");
+passthru("drush deploy:hook");
 echo "Updates complete.\n";
 // Maintenance off.
 echo "Come out of maintenance mode\n";
-passthru("$drush sset system.maintenance_mode 0 -y");
+passthru("drush sset system.maintenance_mode 0 -y");
 echo "All deployment tasks are complete.\n";
